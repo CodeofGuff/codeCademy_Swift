e9eb4568461fd40dc95846982e845d3edca11888
CodeofGuff <guffcode@gmail.com>
2024-07-03 00:12:35 -0700

    We learned functions

--- Functions.swift
+++ Functions.swift
@@ -250,3 +250,67 @@ print(currentGeneratorState)
 // Default Parameter: a parameter that sets a value to the parameter that will be used if an argument is omitted.
 // Variadic Parameter: a parameter that accepts zero or more values of the parameter’s type.
 // In-out Parameter: a parameter whose value is passed into a function and modified within the body.
+
+
+// Rock Paper Scissors
+
+func getUserChoice(userInput: String) -> String {
+  if userInput == "rock" || userInput == "paper" || userInput == "scissors" {
+    return userInput
+  }
+  else {
+    return "You can only enter rock, paper, or scissors. Try again."
+  }
+}
+print(getUserChoice(userInput: "hello"))
+
+func getComputerChoice() -> String {
+  let randomNumber: Int = Int.random(in: 0...2)
+  switch randomNumber {
+    case 0:
+      return "rock"
+    case 1:
+      return "paper"
+    case 2:
+      return "scissors"
+    default:
+      return "Something went wrong"
+  }
+}
+print(getComputerChoice())
+
+func determineWinner(_ userChoice: String, _ compChoice: String) -> String {
+  var decision = "It's a tie"
+  switch userChoice {
+    case "rock":
+      if compChoice == "paper" {
+        decision = "The computer won"
+      }
+      else if compChoice == "scissors" {
+        decision = "The user won"
+      }
+    case "paper":
+      if compChoice == "rock" {
+        decision = "The user won"
+      }
+      else if compChoice == "scissors" {
+        decision = "The computer won"
+      }
+    case "scissors":
+      if compChoice == "rock" {
+        decision = "The computer won"
+      }
+      else if compChoice == "paper" {
+        decision = "The user won"
+      }
+    default:
+      return "Something went wrong"
+  }
+  return decision
+}
+
+let userChoice = getUserChoice(userInput: "paper")
+let compChoice = getComputerChoice()
+print("You threw \(userChoice)")
+print("The computer threw \(compChoice)")
+print(determineWinner(userChoice, compChoice))
