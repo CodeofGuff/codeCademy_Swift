b76f943112af7b96d67b23a7e5812f05601bb8fa
CodeofGuff <guffcode@gmail.com>
2024-07-07 18:57:50 -0700

    I need to learn this part of swift
    before i keep going with iOS dev

---
+++ 9 AccessControl.swift
@@ -0,0 +1,7 @@
+
+
+// Access Control - Makes code dsafer and robust
+// allows us to control access to properties and methos within our code
+// by implementing different levels of access on properties and methods, 
+// we determine which other parts of the code can read their values, write to them, and execute their methods
+
