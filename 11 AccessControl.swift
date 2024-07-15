cf0f7d9af423426e5c462592b0f810e21649d6be
CodeofGuff <guffcode@gmail.com>
2024-07-14 23:48:06 -0700

    yo

--- 11 AccessControl.swift
+++ 11 AccessControl.swift
@@ -1,6 +1,6 @@
 
 
-// Access Control - Makes code dsafer and robust
+// Access Control - Makes code safer and robust
 // allows us to control access to properties and methos within our code
 // by implementing different levels of access on properties and methods, 
 // we determine which other parts of the code can read their values, write to them, and execute their methods
