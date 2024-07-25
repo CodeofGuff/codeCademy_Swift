0c13b58f7d7f71a4c2671fcab4d21bbf24a4c720
CodeofGuff <guffcode@gmail.com>
2024-07-25 00:23:25 -0700

    Learningggg

--- 10 Optionals.swift
+++ 10 Optionals.swift
@@ -192,3 +192,106 @@ if let vicePresidentNameLength = studentGovernment["vicePresident"]?.count {
 // Optionals and Functions
 // optionals are types just like Array and String, we can use them in the signature of a function
 // usefull as there may not always be a good value to return
+
+
+let words = ["optionals", nil, "are", "great", nil, "at", "handling", nil, "values"]
+
+func firstIndex(of target: String, in arr: [String?]) -> Int? {
+  for i in arr.indices {
+    guard let value = arr[i], value == target else { continue }
+    return i
+  }
+  return nil
+}
+
+if let greatIndex = firstIndex(of: "great", in: words) {
+  print(greatIndex)
+}
+
+if let badIndex = firstIndex(of: "bad", in: words) {
+  print(badIndex)
+} else {
+  print("words does not contain the string 'bad'")
+}
+
+
+// REVIEW 
+// Great work! By using optionals, you can make sure to handle values that might be nil. 
+// A value might be nil because the user didn’t provide a response. 
+// A value might also be nil because we might not have a good answer to a question (e.g. what’s the biggest number in an empty list).
+// In this lesson you learned how to:
+// Define optionals
+// Force unwrap optionals using the ! operator
+// Bind optionals using if let statements
+// Bind optionals using guard statements
+// Use the nil-coalescing operator ?? to provide default values
+// Chain optionals using the ? operator
+// Define functions that take in and return optionals
+
+var emptyArr: [Int] = []
+
+print(emptyArr.first ?? "No first value")
+print(emptyArr.last ?? "No last value")
+print(emptyArr.randomElement() ?? "No random value")
+print(emptyArr.max() ?? "No maximum value")
+print(emptyArr.min() ?? "No minimum value")
+print(emptyArr.firstIndex(of: 4) ?? "4 is not in the array")
+print(emptyArr.popLast() ?? "No values to pop from an empty array")
+
+
+// School Database
+//
+struct Teacher {
+    let id: Int
+    let name: String
+    var hairColor: String?
+    var email: String?
+}
+
+struct Student {
+    let name: String
+    let favoriteTeacherID: Int
+}
+
+struct School {
+    let name: String
+    let teachers: [Teacher]
+
+    func getTeacher(withID id: Int) -> Teacher? {
+        for teacher in teachers {
+            if teacher.id == id {
+                return teacher
+            }
+        }
+        return nil
+    }
+    
+    func printFavoriteTeacherInfo(for student: Student) {
+        guard let favoriteTeacher = getTeacher(withID: student.favoriteTeacherID) else {
+            print("No teacher with the id \(student.favoriteTeacherID) works at \(name)!")
+            return
+        }
+        print("\(student.name)'s favorite teacher is \(favoriteTeacher.name)")
+        print("They have \(favoriteTeacher.hairColor ?? "no") hair")
+        guard let email = favoriteTeacher.email else {
+            print("They don't have an email address on file")
+            return
+        }
+        print("Their email address is \(email)")
+    }
+}
+
+
+let teachers = [
+    Teacher(id: 592, name: "Mr. Weatherbee", hairColor: nil, email: "thebee@riverdale.com"),
+    Teacher(id: 481, name: "Ms. Grundy", hairColor: "white", email: nil),
+    Teacher(id: 688, name: "Mr. Adams", hairColor: "black", email: "arthurAdams@riverdale.com")
+]
+
+let riverdaleHigh = School(name: "Riverdale High", teachers: teachers)
+
+let archie = Student(name: "Archie Andrews", favoriteTeacherID: 481)
+let betty = Student(name: "Betty Cooper", favoriteTeacherID: 443)
+
+riverdaleHigh.printFavoriteTeacherInfo(for: archie)
+riverdaleHigh.printFavoriteTeacherInfo(for: betty)
